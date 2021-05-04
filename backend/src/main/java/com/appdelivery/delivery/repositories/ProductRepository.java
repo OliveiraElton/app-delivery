e6b979993e682280d8fea4f86b1cb0fd37ffd257
Elton Francisco de Oliveira <eltonf.oliveira22@gmail.com>
2021-05-03 21:30:18 -0300

    alterado productRepository and productService

--- backend/src/main/java/com/appdelivery/delivery/repositories/ProductRepository.java
+++ backend/src/main/java/com/appdelivery/delivery/repositories/ProductRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-//    List<Product> findAllByOrderNameAsc();
+    List<Product> findAllByOrderByNameAsc();
 }
